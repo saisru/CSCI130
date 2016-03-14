5923139fdeeba7524f903d8ae4de1245befd924d
yellapati <sruthi.vs278@gmail.com>
2016-03-13 22:59:46 -0700

    go

---
+++ Project/project step3/main.go
@@ -0,0 +1,37 @@
+
+
+package main
+import (
+		"net/http"					
+		"html/template" 				
+		"io/ioutil"						
+		"github.com/nu7hatch/gouuid"				
+		"strings"						
+ 		"fmt" 									
+)
+func main() {
+	tpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		name := req.FormValue("name")
+		age := req.FormValue("age")
+		cookie, err := req.Cookie("session-fino")
+		if err != nil {
+			id, _ := uuid.NewV4()
+			cookie = &http.Cookie{
+				Name:  "session-fino",
+				Value: id.String() + "|" + name + age,
+				// Secure: true,
+				HttpOnly: true,
+			}
+			http.SetCookie(res, cookie)
+		}
+		err = tpl.Execute(res, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
